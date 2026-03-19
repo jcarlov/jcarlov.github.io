a34e0b7b8bcbf0cd20f71eaffa2999591cd02d70
jcarlov <122693438+jcarlov@users.noreply.github.com>
2026-03-19 22:54:52 +0000

    Deploying to gh-pages from @ jcarlov/jcarlov.github.io@f89fc008554c60468e370dbc3a9f2bde516cfa43 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,17 +46,17 @@ ninja.data = [{
         },
       },{
         id: 'social-github',
-        title: 'Github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("", "_blank");
+          window.open("https://github.com/jcarlov", "_blank");
         },
       },{
         id: 'social-linkedin',
-        title: 'Linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("", "_blank");
+          window.open("https://www.linkedin.com/in/jeancarlo-velez", "_blank");
         },
       },{
       id: 'light-theme',
