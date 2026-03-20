3901aa7fd13d0bcf72ee895d4bf7a0e636387a2e
jcarlov <122693438+jcarlov@users.noreply.github.com>
2026-03-20 02:22:23 +0000

    Deploying to gh-pages from @ jcarlov/jcarlov.github.io@c81de60aa0cef8df44bc78339f5dfe8081603b63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "Draft — updated periodically.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
